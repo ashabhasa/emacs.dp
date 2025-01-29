523958ee97c9d2638e263a127e98a206e6a8850a
Arber Shabhasa <ashabhasa@gmail.com>
2025-01-29 16:51:59 +0100

    config for racket

--- lisp/init-racket.el
+++ lisp/init-racket.el
@@ -6,5 +6,14 @@
   (unless (package-installed-p 'racket-mode)
     (package-install 'racket-mode)))
 
+
+(require 'racket-xp)
+(add-hook 'racket-mode-hook #'racket-xp-mode)
+(add-hook 'racket-xp-mode-hook
+          (lambda ()
+            (remove-hook 'pre-redisplay-functions
+                         #'racket-xp-pre-redisplay
+                         t)))
+
 (provide 'init-racket)
 ;; init-racket.el ends here
