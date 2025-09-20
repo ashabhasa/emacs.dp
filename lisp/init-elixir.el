a66bef574f4f5930235fbf55407535c2127da64a
Arber Shabhasa <ashabhasa@gmail.com>
2025-09-20 15:49:48 +0200

    Try out new lsp for elixir

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -23,6 +23,7 @@
 (add-hook 'elixir-ts-mode-hook 'eglot-ensure)
 
 (add-to-list 'eglot-server-programs '(elixir-ts-mode  "~/dev/elixir/elixir-ls-gh/release_03_04_2025/language_server.sh"))
+;; (add-to-list 'eglot-server-programs '(elixir-ts-mode  "~/dev/elixir/expert/apps/expert/burrito_out/expert_darwin_arm64"))
 
 (unless (package-installed-p 'exunit)
   (package-install 'exunit))
